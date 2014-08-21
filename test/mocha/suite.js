9d0557c1dfbd6e9303b3edd3b94124a6789c7b5b
david kaye <dfkaye@yahoo.com>
2014-08-21 13:07:56 -0700

    add todo api comments before diving in

--- test/mocha/suite.js
+++ test/mocha/suite.js
@@ -1,5 +1,7 @@
 // mocha/suite.js
 
+var assert = assert;
+
 if (typeof require == 'function') {
   require('../../../stringtemplate');
 
@@ -14,3 +16,14 @@ test('exists', function () {
   assert(typeof ''.template == 'function');
   assert(typeof (function(){}).template == 'function');
 });
+
+/*
+  template() accepts 
+  + string
+  + object
+  + array of strings or objects
+  
+  template() returns
+  + ?monad? with a .toString() method that returns the finished string
+  + string
+*/
