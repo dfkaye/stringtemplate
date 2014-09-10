956f8f7e4cbe3319b84d6fbfeca19402dc389709
david kaye <dfkaye@yahoo.com>
2014-09-10 12:54:56 -0700

    string#template base case tests added

--- stringtemplate.js
+++ stringtemplate.js
@@ -1,11 +1,189 @@
 // stringtemplate.js
 
+// Sept 4-5, 2014 ~ start the zero-recursion version 
+// Sept 6 - still in progress but most array drilldown is done
+// Sept 9 ~ cleaned up array v placeholder detection and drilldown
+
+// TODO ~ BUILD REPO WITH TESTS, DOCUMENTATION, EXAMPLES
+
+
+// 21 August 2014 1:04 AM PDT
+/*
+// test
+
+'<p>notes & quotes</p>\n\r<q>$who$ said, "I\'ll be back"</q>'.escapeHtml();
+// => "&lt;p&gt;notes &amp; quotes&lt;/p&gt;\n\r&lt;q&gt;$who$ said, &quot;I\'ll be back&quot;&lt;/q&gt;"
+*/
+var escapes = {"\\": "\\\\", "\n": "\\n", "\r": "\\r", "'": "\\'"},
+    entities = {'&': '&amp;', '"': '&quot;', '<': '&lt;', '>': '&gt;'};
+
+typeof String.prototype.escapeHtml == 'function' || 
+(String.prototype.escapeHtml = function escapeHtml() {
+  return this
+  .replace(/[\\\n\r']/g, function(char) {
+    return escapes[char];
+  })
+  .replace(/[&\"<>]/g, function(char) {
+    return entities[char];
+  });
+});
+
+/////////////////////////////////////////////
+
+// string#trim polyfill
+typeof String.prototype.trim == 'function' ||
+(String.prototype.trim = function trim() {
+  return this.replace(/^\s+|\s+$/gm, '');
+});
+
+// string#template
+// a batch string#replace, using $token$ placeholders for values/objects
+// and @arrayName@ + @/@ tokens for indexed data (arrays)
+// requires string#trim
 typeof String.prototype.template == 'function' ||
-(String.prototype.template = function template() {
+(String.prototype.template = function template(data) {
 
+  if (!this.match(/\$[^\$]+\$/g) || !data) {
+    return this;
+  }
+
+  var rows = this.split('\n'),
+      parts = [],     // output collector
+      i, j, k, p, t,  // loop indexes
+      row,            // current row under process
+      placeholder,    // match for $name$ or $data.name$:
+      arrayStart,     // match for @array@ or @data.array@
+      arrayEnd,       // match for @/@
+      pathname, tokens, block, array, item;
+      
+  for (i = 0; i < rows.length; i += 1) {
+  
+    row = rows[i];
+    
+    placeholder = row.match(/\$[^\$]+\$/g);
+    arrayStart = row.match(/\@[^\@^\/]+\@/g);
+    //arrayEnd = row.match(/\@\/\@/g);
+
+    // @array@ || @data.array@:
+    if (arrayStart) {
+      arrayEnd = false;
+      array = data;
+      pathname = arrayStart[0].replace(/\@/g, ''); // console.log('pathname: ' + pathname);
+      
+      if (pathname != '.') {
+      
+        // drill-down name tokens
+        tokens = pathname.split('.');
+        p = 0;
+        
+        while (p < tokens.length) {
+          array = array[tokens[p]];
+          p += 1;
+        }
+      }
+
+      block = [];
+      
+      // add rows to block until next array-end token, @/@ 
+      while (rows[i += 1] && !rows[i].match(/\@\/\@/)) {
+        
+        if (i ===  rows.length - 1) {
+        
+          // rather than throw an error, return this message as the result
+          
+          return 'Error: closing @/@ tag for ' + arrayStart[0] + ' array not found';
+        }
+        
+        block.push(rows[i].trim());
+        
+      } // console.log(block);
+      
+      // process copies of each row in block
+      for (j = 0; j < array.length; j += 1) {
+      
+        row = block.join('\n');
+        item = array[j];
+        
+        if (~row.indexOf('$.$')) {
+          // solve array index value $.$
+          row = row.replace('$.$', item);
+        } else if (typeof item == 'object') {
+          // solve item as key:value object
+          for (var k in item) {
+            row = row.replace('$'+k+'$', item[k]);
+          }
+        }
+        
+        parts.push(row.trim());
+      }
+      
+    } else {
+    
+      // $name$ || $data.name$:
+      if (placeholder) {
+      
+        // iterate placeholder in current row // console.log('placeholder: ' + placeholder)
+        for (t = 0; t < placeholder.length; t += 1) {
+
+          item = data;
+                    
+          // drill-down name tokens
+          tokens = placeholder[t].replace(/\$/g, '').split('.');
+          p = 0;
+
+          while (p < tokens.length) {
+            item = item[tokens[p]];
+            p += 1;
+          }
+
+          row = row.replace(placeholder[t], item);
+        }
+      }
+      
+      // avoid array start/end, and whitespace-only
+      //if (!row.match(/\@/g) && !row.match(/^\s*$/)) {
+        parts.push(row.trim());
+      //}      
+    }   
+  }
+
+  return parts.join('\n').trim();
 });
 
+// function#template
+// heredoc/multiline string polyfill
+// requires string#trim and string#template
 typeof Function.prototype.template == 'function' ||
-(Function.prototype.template = function template() {
+(Function.prototype.template = function template(data) {
 
+  var fs = String(this);
+  
+  // splitting logic taken from where.js
+  var fnBody = fs.replace(/\s*function[^\(]*[\(][^\)]*[\)][^\{]*{/,'')
+                 .replace(/[\}]$/, '');
+  var table = fnBody.match(/\/(\*){3,3}[^\*]+(\*){3,3}\//);
+  var rows = (table && table[0] || fs)
+              .replace(/\/\/[^\r]*/g, '') // remove line comments...
+              .replace(/(\/\*+)*[\r]*(\*+\/)*/g, '') // ...block comments
+              .split('\n'); // and split by newline
+              
+  var r = [];
+  
+  for (var i = 0; i < rows.length; i++) {
+    if (!rows[i].match(/^\s*$/)) {
+      r.push( rows[i].trim() );
+    }
+  }
+    
+  r = r.join('\n').trim();
+  
+  return (data && typeof data == 'object') ? r.template(data) : r;
 });
+
+
+
+
+
+
+
+
--- test/mocha/suite.js
+++ test/mocha/suite.js
@@ -4,22 +4,190 @@ var assert = assert;
 
 if (typeof require == 'function') {
   require('../../../stringtemplate');
-
   assert = require('assert');
 }
 
 /* TESTS START HERE */
 
-suite('smoke test');
+suite('string#template');
 
-test('exists', function () {
+test('is method', function () {
   assert(typeof ''.template == 'function');
+});
+
+test('returns same when argument is not an object', function () {
+  var s = '';
+  
+  assert(s.template().toString() === s.toString());
+  assert(s.template(1).toString() === s.toString());
+  assert(s.template(true).toString() === s.toString());
+});
+
+test('returns same when argument is an empty object or array', function () {
+  var s = '';
+  
+  assert(s.template(null).toString() === s.toString());
+  assert(s.template({}).toString() === s.toString());
+  assert(s.template([]).toString() === s.toString());  
+});
+
+test('replaces matched $placeholder$ data', function () {
+  var s = '<p>$value$</p>';
+  
+  assert(s.template({ value: 'placeholder' }).toString() === '<p>placeholder</p>');
+});
+
+test('replaces un-matched $placeholder$ data with undefined', function () {
+  var s = '<p>$value$</p>';
+  
+  assert(s.template({ wrongName: 'placeholder' }).toString() === '<p>undefined</p>');
+});
+
+test('replaces $nested.placeholder$ data', function () {
+  var s = '<p>$nested.value$</p>';
+  
+  assert(s.template({ 
+    nested: {
+      value: 'placeholder' 
+    }
+  }).toString() === '<p>placeholder</p>');
+});
+
+test('returns item values in multiple rows when data argument is array using @.@ $name$ @/@', function () {
+  var s = ['@.@', '<li>$name$</li>', '@/@'].join('\n');
+  
+  assert(s.template([
+    { name: 'charlize' },
+    { name: 'zora neale' }
+  ]).toString() === ['<li>charlize</li>', '<li>zora neale</li>'].join('\n'));
+});
+
+test('returns index values in multiple rows when data argument is array using @.@ $.$ @/@', function () {
+  var s = ['@.@', '<li>$.$</li>', '@/@'].join('\n');
+  
+  assert(s.template([
+    33, false
+  ]).toString() === ['<li>33</li>', '<li>false</li>'].join('\n'));
+});
+
+test('replaces nested array index values using @array@ $.$ @/@', function () {
+  var s = ['@array@', '<li>$.$</li>', '@/@'].join('\n');
+  
+  assert(s.template({ 
+    array: [ 'three', 'four', 'five' ]
+  }).toString() === ['<li>three</li>', '<li>four</li>', '<li>five</li>'].join('\n'));
+});
+
+test('replaces nested array item keynames using @array@ $item$ @/@', function () {
+  var s = ['@array@', '<li>$item$</li>', '@/@'].join('\n');
+  
+  assert(s.template({ 
+    array: [
+      { item: 'one' },
+      { item: 'two' }
+    ]
+  }).toString() === ['<li>one</li>', '<li>two</li>'].join('\n'));
+});
+
+test('@array@ returns empty string when template does not contain newline \\n chars', function () {
+  var s = ['@array@', '<li>$item$</li>', '@/@'].join();
+  
+  assert(s.template({ 
+    array: [
+      { item: 'one' },
+      { item: 'two' }
+    ]
+  }).toString() === '');
+});
+
+test('@array@ returns error Message (does not throw) when template does not contain closing @/@ tag', function () {
+  var s = ['@missingEndTag@', '<li>$item$</li>'].join('\n');
+  
+  assert(s.template({ 
+    missingEndTag: [
+      { item: 'one' },
+      { item: 'two' }
+    ]
+  }).toString() == 'Error: closing @/@ tag for @missingEndTag@ array not found');
+});
+
+
+
+suite('function#template');
+
+test('function#template is method', function () {
   assert(typeof (function(){}).template == 'function');
 });
 
+// test('function#template is method', function () {
+  // assert(typeof (function(){}).template == 'function');
+// });
+
+// test('function#template is method', function () {
+  // assert(typeof (function(){}).template == 'function');
+// });
+
 /*
+  // simple
+  var label = '<p>$label$</p>';
+  label.template({ label: 'my addresses' }); // => <p>my addresses</p>
+
+  // list
+  var list = '<ul>$addresses$</ul>';
+  var address = '<li>$street$</li><li>$city$, $state$</li>';
+
+  var t = list.template({ 
+    addresses: address.template([
+      { street: '123 fourth street', city: 'cityburgh', state: 'aa' },
+      { street: '567 eighth street', city: 'burgville', state: 'bb' },
+      { street: '910 twelfth street', city: 'villetown', state: 'cc' }
+    ])
+  });
+  console.log(t == '<ul><li>123 fourth street</li><li>cityburgh, aa</li>' + '\n' +
+                    '<li>567 eighth street</li><li>burgville, bb</li>' + '\n' +
+                    '<li>910 twelfth street</li><li>villetown, cc</li></ul>')
+
+  // css
+  
+  var selector = '$selector$';
+  var ruleset = '{ $ruleset$ }';
+  var rule = '$prop$: $value$;';
+
+  var r = rule.template([ {prop: 'color', value: '#345'} ])
+  var rs = ruleset.template({ ruleset: r });
+  var s = selector.template({selector: 'a > b > c'}).template(rs);
+  console.log(s == 'a > b > c{ color: #345; }');
+
+  // heredoc css
+
+  function cssruleset() {
+    /\***
+    $selector$ { 
+      $declarations$
+    }
+    ***\/
+  }
+
+  function declarations() {
+    /\***
+     basic: 10px;
+     color: $color$;
+     background-color: $bgcolor$;
+    ***\/
+  }
+
+  cssruleset.template({
+    selector: ".class ~ whatever::after",
+    declarations: declarations.template({
+      color: '#345',
+      bgcolor: 'rgb(34, 0, 53)'
+    })
+  });
+
+
   template() accepts 
-  + string
+  
+  + string 
   + object
   + array of strings or objects
   
